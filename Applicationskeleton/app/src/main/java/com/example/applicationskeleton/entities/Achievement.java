213e238be3efee7b750e27525d22cecc62633157
jmytkh <79437616+jmytkh@users.noreply.github.com>
2022-03-22 21:56:14 +0800

    jere1

--- Applicationskeleton/app/src/main/java/com/example/applicationskeleton/entities/Achievement.java
+++ Applicationskeleton/app/src/main/java/com/example/applicationskeleton/entities/Achievement.java
@@ -5,4 +5,53 @@ public class Achievement {
     private int count;
     private int rank;
     private int toRankUp;
+
+
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     *
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCount() {
+        return this.count;
+    }
+
+    /**
+     *
+     * @param count
+     */
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getRank() {
+        return this.rank;
+    }
+
+    /**
+     *
+     * @param rank
+     */
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    public int getToRankUp() {
+        return this.toRankUp;
+    }
+
+    /**
+     *
+     * @param toRankUp
+     */
+    public void setToRankUp(int toRankUp) {
+        this.toRankUp = toRankUp;
+    }
 }
--- Applicationskeleton/app/src/main/java/com/example/applicationskeleton/entities/LoginStreak.java
+++ Applicationskeleton/app/src/main/java/com/example/applicationskeleton/entities/LoginStreak.java
@@ -1,4 +1,35 @@
 package com.example.applicationskeleton.entities;
 
+import java.time.LocalDateTime;
+
 public class LoginStreak extends Achievement {
+    private int loginStreak;
+    private LocalDateTime lastLogin;
+    public void getLoginStreak() {
+        // TODO - implement achivement.getLoginStreak
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     *
+     * @param LoginStreak
+     */
+    public void setLoginStreak(int LoginStreak) {
+        this.loginStreak = LoginStreak;
+    }
+
+    public void getLastLogin() {
+        // TODO - implement achivement.getLastLogin
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     *
+     * @param LastLogin
+     */
+    public void setLastLogin(int LastLogin) {
+        // TODO - implement achivement.setLastLogin
+        throw new UnsupportedOperationException();
+    }
+
 }
