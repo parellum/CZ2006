1c8cd953110cdf64a60ae65b49f4363bd79336de
wangsishi <544wangsishi@gmail.com>
2022-03-22 23:04:38 +0800

    ss

--- Applicationskeleton/app/src/main/java/com/example/applicationskeleton/entities/Facility.java
+++ Applicationskeleton/app/src/main/java/com/example/applicationskeleton/entities/Facility.java
@@ -5,7 +5,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class Facility {
-    private int facilityID;
+    private String facilityID; //changed from int to String
     private String name;
     private String description;
     private int postalCode;
@@ -19,4 +19,51 @@ public class Facility {
     private ArrayList<Integer> ratingScoreList;
     private int numTotalRating;
     private ArrayList<Review> reviewList;
+
+    public String getFacilityID() {
+        return facilityID;
+    }
+
+    public void setFacilityID(String facilityID) {
+        this.facilityID = facilityID;
+    }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public int getPostalCode() { return postalCode; }
+
+    public void setPostalCode(int postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public LocalDateTime getOpeningHour() {
+        return this.openingHour;
+    }
+
+    public void setOpeningHour(LocalDateTime openingHour) {
+        this.openingHour = openingHour;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public ArrayList<Integer> getRating() {
+        return ratingScoreList;
+    }
+
+    public int getNumTotalRating() {
+        return numTotalRating;
+    }
+
+    public void setNumTotalRating(int numTotalRating) {
+        this.numTotalRating = numTotalRating;
+    }
+
 }
