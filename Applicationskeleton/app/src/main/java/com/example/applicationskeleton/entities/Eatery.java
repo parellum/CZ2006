995190563b17d615a75899565b109350227004c4
huijingyam <oliviayam0921@gmail.com>
2022-03-22 22:58:37 +0800

    eatery and food class

--- Applicationskeleton/app/src/main/java/com/example/applicationskeleton/entities/Eatery.java
+++ Applicationskeleton/app/src/main/java/com/example/applicationskeleton/entities/Eatery.java
@@ -3,7 +3,82 @@ package com.example.applicationskeleton.entities;
 import java.util.ArrayList;
 
 public class Eatery extends Facility{
+    /**
+     * Denotes typeOfFood served at the eatery, the foodID
+     * and the list of food available in the eatery
+     */
+
     private String typeOfFood;
-    private int foodID;
+    private int foodID; //do we need foodID here?
     private ArrayList<Food> listOfFood;
+
+    /**
+     * Creates a new eatery object using the following fields
+     * @param facilityID
+     * @param name
+     * @param description
+     * @param postalCode
+     * @param openingHour
+     * @param contactNumber
+     * @param priceRange
+     * @param emailAddres
+     * @param password
+     * @param eateryAddress
+     * @param numberOfClicks
+     * @param ratingScoreList
+     * @param numTotalRating
+     * @param reviewList
+     * @param typeOfFood
+     * @param foodID
+     * @param listOfFood
+     */
+    public Eatery(int facilityID, String name,String description, int postalCode,
+                  LocalDateTime openingHour, String contactNumber, String priceRange,
+                  String emailAddres,String password, String eateryAddress,  int numberOfClicks,
+                  ArrayList<Integer> ratingScoreList, int numTotalRating,
+                  ArrayList<Review> reviewList,
+                  String typeOfFood, int foodID, ArrayList<Food> listOfFood){
+
+        super(facilityID, name, description, postalCode, openingHour, contactNumber,
+                priceRange, emailAddres, password, eateryAddress, numberOfClicks,
+                ratingScoreList, numTotalRating, reviewList);
+
+        this.typeOfFood = typeOfFood;
+        this.foodID = foodID;
+        this.listOfFood = listOfFood;
+    }
+
+
+    /**
+     * Display all information related to the eatery
+     */
+    public void display() { //refer to facility display()
+        super.display();
+        System.out.println(String.format("%-30s%-75s%-10s", typeOfFood,foodID,listOfFood));
+    }
+
+    /**
+     * Takes in the information like type of food, foodID and list of food
+     * provided by user and updates the typeOfFood, foodID, listOfFood fields
+     * @param typeOfFood
+     * @param foodID
+     * @param listOfFood
+     */
+    /**
+     *
+     * @param typeOfFood
+     */
+
+    public void setTypeOfFood(String typeOfFood){
+        this.typeOfFood = typeOfFood;
+    }
+
+    public void setFoodID(int foodID){
+        this.foodID = foodID;
+    }
+
+    public void setListOfFood(ArrayList<Food> listOfFood){
+        this.listOfFood = listOfFood;
+    }
 }
+
