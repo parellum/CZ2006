de0808ff1fccf74ec40ac72400a2b4e641678ad9
wangsishi <544wangsishi@gmail.com>
2022-03-31 22:00:52 +0800

    status scrollview

--- Fitrition/app/src/main/java/com/example/fitrition/MainActivity.java
+++ Fitrition/app/src/main/java/com/example/fitrition/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.fitrition;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.fitrition.boundary.RegisterActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,13 +18,121 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.fitrition.databinding.ActivityMainBinding;
 
-public class MainActivity extends FragmentActivity {
-
+public class MainActivity extends FragmentActivity implements View.OnClickListener{
+    Button status, status1, status2, status3, status4, status5, status6,status7, status8, status9, status10;
     private ActivityMainBinding binding;
+    private Button friendStatus;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        friendStatus=(Button) findViewById(R.id.friendSatusBtn);
+        friendStatus.setOnClickListener((View.OnClickListener) this);
+
+        status1 = (Button)findViewById( R.id.btn1);
+        status2 = (Button)findViewById( R.id.btn2 );
+        status3 = (Button)findViewById( R.id.btn3 );
+        status4 = (Button)findViewById( R.id.btn4 );
+        status5 = (Button)findViewById( R.id.btn5 );
+        status6 = (Button)findViewById( R.id.btn6 );
+        status7 = (Button)findViewById( R.id.btn7 );
+        status8 = (Button)findViewById( R.id.btn8 );
+        status9 = (Button)findViewById( R.id.btn9 );
+        status10 = (Button)findViewById( R.id.btn10 );
+
+        status1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Activity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+        status2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Activity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+        status3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Activity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+        status4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Activity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+        status5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Activity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+        status6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Activity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+        status7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Activity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+        status8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Activity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+        status9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Activity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+        status10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Activity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.friendSatusBtn:
+
+                break;
+            case R.id.personal:
+                break;
+        }
     }
 
 }
--- Fitrition/app/src/main/java/com/example/fitrition/boundary/FriendFragment.java
+++ Fitrition/app/src/main/java/com/example/fitrition/boundary/FriendFragment.java
@@ -1,5 +1,22 @@
 package com.example.fitrition.boundary;
 
-public class FriendFragment {
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewDebug;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.example.fitrition.R;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
+
+public class FriendFragment extends Fragment implements View.OnClickListener{
+    
 
 }
