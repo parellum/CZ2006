a19cba3da012074e3e975c84bb4cf392bc25ab56
Jac <80085511+parellum@users.noreply.github.com>
2022-03-31 20:44:24 +0800

    Main Activity Done

--- Fitrition/app/src/main/java/com/example/fitrition/MainActivity.java
+++ Fitrition/app/src/main/java/com/example/fitrition/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.fitrition;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.example.fitrition.boundary.CalendarFragment;
+import com.example.fitrition.boundary.ExploreFragment;
+import com.example.fitrition.boundary.ProfileFragment;
+import com.example.fitrition.boundary.SocialFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -20,6 +27,35 @@ public class MainActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.main_bot_nav);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener= new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment=null;
+
+            switch (item.getItemId()){
+                case R.id.nav_explore:
+                    selectedFragment=new ExploreFragment();
+                    break;
+                case R.id.nav_calendar:
+                    selectedFragment=new CalendarFragment();
+                    break;
+                case R.id.nav_social:
+                    selectedFragment=new SocialFragment();
+                    break;
+                case R.id.nav_profile:
+                    selectedFragment=new ProfileFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment_container,selectedFragment).commit();
+
+            return true;
+        }
+    };
 
 }
