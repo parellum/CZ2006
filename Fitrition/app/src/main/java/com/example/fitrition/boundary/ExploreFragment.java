1a8bfd8aaead4cea26f8594a6ea06330e5548968
Jac <80085511+parellum@users.noreply.github.com>
2022-04-01 03:44:28 +0800

    Map

--- Fitrition/app/src/main/java/com/example/fitrition/boundary/ExploreFragment.java
+++ Fitrition/app/src/main/java/com/example/fitrition/boundary/ExploreFragment.java
@@ -9,12 +9,37 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.fitrition.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class ExploreFragment extends Fragment {
+public class ExploreFragment extends Fragment implements OnMapReadyCallback {
+
+    MapView map;
+    GoogleMap gmap;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_explore, container, false);
+        View view = inflater.inflate(R.layout.fragment_explore, container, false);
+        map=(MapView) view.findViewById(R.id.mapView);
+        map.onCreate(savedInstanceState);
+        map.getMapAsync(this);
+        map.onResume();
+
+        return view;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap){
+        gmap=googleMap;
+        LatLng Garden = new LatLng(1.279689, 103.862667);
+        gmap.addMarker(new MarkerOptions().position(Garden).title("Garden"));
+        gmap.moveCamera(CameraUpdateFactory.newLatLng(Garden));
+
     }
 }
