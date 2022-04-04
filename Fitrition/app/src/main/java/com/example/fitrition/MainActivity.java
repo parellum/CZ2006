68c489c8cfbaaaecbace22828c0ee969db00b573
huijingyam <oliviayam0921@gmail.com>
2022-04-04 21:30:20 +0800

    help page

--- Fitrition/app/src/main/java/com/example/fitrition/MainActivity.java
+++ Fitrition/app/src/main/java/com/example/fitrition/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.fitrition;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -13,9 +15,11 @@ import com.example.fitrition.boundary.ExploreFragment;
 import com.example.fitrition.boundary.ProfileFragment;
 import com.example.fitrition.boundary.SocialFragment;
 import com.example.fitrition.databinding.ActivityMainBinding;
+import com.example.fitrition.utils.HelpActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
@@ -25,48 +29,57 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
     private Toolbar mToolbar;
-    private ImageView backIcon;
+
+//    private Button btnActivityMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment_container,new ExploreFragment()).commit();
         BottomNavigationView bottomNav = findViewById(R.id.main_bot_nav);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
 
+        //trying out without toolbar
         mToolbar = findViewById(R.id.main_toolbar);
         setSupportActionBar(mToolbar);
 
-        backIcon = findViewById(R.id.back_icon);
 
-        backIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Back selected", Toast.LENGTH_SHORT).show();
-            }
-        });
+//        ActionBar actionBar = getSupportActionBar();
+//        actionBar.setTitle("  Fitrition");
+//        actionBar.setIcon(R.mipmap.fitrition_logo);
+//        actionBar.setDisplayUseLogoEnabled(true);
+//        actionBar.setDisplayShowHomeEnabled(true);
+
+
+
+
+// this is used when we want to click a BUTTON and it goes to the other activity
+//        btnActivityMain = (Button)findViewById(R.id.help);
+//        btnActivityMain.setOnClickListener((v)->{
+//            startActivity(new Intent(MainActivity.this, HelpActivity.class));
+//        });
     }
-
-
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
+
         getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item) {
         switch(item.getItemId()){
             case R.id.help:
-                Toast.makeText(this, "Help selected", Toast.LENGTH_SHORT).show();
-                return true;
+                startActivity(new Intent(MainActivity.this, HelpActivity.class));
+//                Toast.makeText(this, "Help selected", Toast.LENGTH_SHORT).show();
+                break;
+
             case R.id.log_out:
                 Toast.makeText(this, "Log Out selected", Toast.LENGTH_SHORT).show();
-                return true;
+                break;
         }
         return super.onOptionsItemSelected(item);
     }
