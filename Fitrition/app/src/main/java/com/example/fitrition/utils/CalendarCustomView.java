2ab44e24018bf226d9961ac87a55de35466cfcbf
jmytkh <79437616+jmytkh@users.noreply.github.com>
2022-04-06 23:04:18 +0800

    Revert "Update CalendarCustomView.java"
    This reverts commit 14d467c19b50aec11e684fad3d1ab4901eb8a055.

--- Fitrition/app/src/main/java/com/example/fitrition/utils/CalendarCustomView.java
+++ Fitrition/app/src/main/java/com/example/fitrition/utils/CalendarCustomView.java
@@ -2,13 +2,7 @@ package com.example.fitrition.utils;
 
 import static android.content.Context.LAYOUT_INFLATER_SERVICE;
 
-<<<<<<< Updated upstream
 import android.content.Intent;
-=======
-import android.app.AlertDialog;
-import android.database.sqlite.SQLiteDatabase;
-import android.media.metrics.Event;
->>>>>>> Stashed changes
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.widget.LinearLayout;
@@ -23,7 +17,6 @@ import android.widget.LinearLayout;
         import android.widget.ImageView;
 import android.widget.PopupWindow;
 import android.widget.TextView;
-<<<<<<< Updated upstream
 
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -32,22 +25,6 @@ import androidx.fragment.app.FragmentTransaction;
 
 import com.example.fitrition.MainActivity;
 import com.example.fitrition.boundary.NewEventFragment;
-=======
-import android.widget.Toast;
-import android.database.Cursor;
-
-import androidx.recyclerview.widget.DefaultItemAnimator;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-
-import com.example.fitrition.adapter.EventRecyclerAdapter;
-import com.example.fitrition.adapter.StatusAdapter;
-import com.example.fitrition.boundary.DBOpenHelper;
-import com.example.fitrition.boundary.DBStructure;
-import com.example.fitrition.entities.Events;
-import com.example.fitrition.entities.Status;
->>>>>>> Stashed changes
 import com.example.fitrition.uiReference.tracker.ExpandableHeightGridView;
 import com.example.fitrition.R;
 
@@ -70,16 +47,7 @@ public class CalendarCustomView extends LinearLayout {
     private Calendar cal = Calendar.getInstance(Locale.ENGLISH);
     private Context context;
     private com.example.fitrition.utils.GridAdapter mAdapter;
-<<<<<<< Updated upstream
 
-=======
-    RecyclerView recyclerView;
-    EventRecyclerAdapter eventRecyclerAdapter;
-    ArrayList<Events> eventsList = new ArrayList<>();
-    List<Date> dateList = new ArrayList<>();
-    DBOpenHelper dbOpenHelper;
-    AlertDialog alertDialog;
->>>>>>> Stashed changes
 
     public CalendarCustomView(Context context) {
         super(context);
@@ -91,29 +59,6 @@ public class CalendarCustomView extends LinearLayout {
         setAddEventButtonClickEvent();;
     }
 
-//    public static final String[] eventTest= {"Philip ate Chicken Rice at NTU Hall 15",
-//            "Mathew has achieved a hotstreak of 1000 days!",
-//            "Sam has completed her 9th Swimming session!",
-//            "Richard gave PastaExpress 5/5 stars!" ,
-//            "Philip ate Chicken Rice at NTU Hall 15 again",
-//            "Philip ate Chicken Rice at NTU Hall 15 again",
-//            "Philip ate Chicken Rice at NTU Hall 15 again",
-//            "Philip ate Chicken Rice at NTU Hall 15 again",
-//            "Philip ate Chicken Rice at NTU Hall 15 again",
-//            "Philip ate Chicken Rice at NTU Hall 15 again",
-//            "Philip ate Chicken Rice at NTU Hall 15 again",
-//            "Philip ate Chicken Rice at NTU Hall 15 again",
-//            "Philip ate Chicken Rice at NTU Hall 15 again",
-//            "Philip ate Chicken Rice at NTU Hall 15 again",
-//            "Philip ate Chicken Rice at NTU Hall 15 again",
-//            "Philip ate Chicken Rice at NTU Hall 15 again",
-//            "Philip ate Chicken Rice at NTU Hall 15 again",
-//            "Philip ate Chicken Rice at NTU Hall 15 again",
-//            "Philip ate Chicken Rice at NTU Hall 15 again",
-//            "Philip ate Chicken Rice at NTU Hall 15 again",
-//            "Philip ate Chicken Rice at NTU Hall 15 again",
-//            "Philip ate Chicken Rice at NTU Hall 15 again"}
-
 
     private void initializeUILayout() {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
@@ -124,53 +69,8 @@ public class CalendarCustomView extends LinearLayout {
         addEventButton = (Button) findViewById(R.id.buttonAddEvent);
         calendarGridView = (ExpandableHeightGridView) view.findViewById(R.id.calendar_grid);
         calendarGridView.setExpanded(true);
-<<<<<<< Updated upstream
     }
 
-=======
-
-
-        //EVENTS RECYCLER ADAPTER TO BE DONE BY JEREMY
-//        Events event1 = new Events("Presentation", "1030", "11", "04", "22");
-
-        RecyclerView EventRV= (RecyclerView) view.findViewById(R.id.my_feed);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(view.getContext());
-        EventRV.setLayoutManager(layoutManager);
-        EventRV.setHasFixedSize(true);
-
-        EventRecyclerAdapter eventRecyclerAdapter = new EventRecyclerAdapter(view.getContext()
-                , eventsList);
-        EventRV.setAdapter(eventRecyclerAdapter);
-        eventRecyclerAdapter.notifyDataSetChanged();
-
-        AlertDialog.Builder builder =new AlertDialog.Builder(context);
-        builder.setCancelable(true);
-        alertDialog =builder.create();
-        alertDialog.show();
-    }
-
-    private ArrayList<Events> CollectEvent(String date){
-        ArrayList<Events> arrayList = new ArrayList<>();
-        dbOpenHelper = new DBOpenHelper(context);
-        SQLiteDatabase sqLiteDatabase = dbOpenHelper.getReadableDatabase();
-        Cursor cursor = dbOpenHelper.ReadEvents(date,sqLiteDatabase);
-        while (cursor.moveToNext()){
-            String event = cursor.getString(1);
-            String Time = cursor.getString(2);
-            String Date = cursor.getString(3);
-            String month = cursor.getString(4);
-            String year = cursor.getString(5);
-            Events events = new Events(event,Time,Date,month,year);
-            arrayList.add(events);
-        }
-        cursor.close();
-        dbOpenHelper.close();
-// Toast.makeText(context, String.valueOf(arrayList.size()), Toast.LENGTH_SHORT).show();
-
-        return arrayList;
-    }
-
->>>>>>> Stashed changes
     private void setPreviousButtonClickEvent() {
         previousButton.setOnClickListener(new OnClickListener() {
             @Override
