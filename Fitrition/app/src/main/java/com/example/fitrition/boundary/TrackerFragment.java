3d6b583f9bfe407b06ed2f3d644da3ef4a7c4b3e
jmytkh <79437616+jmytkh@users.noreply.github.com>
2022-04-07 15:07:28 +0800

    Update TrackerFragment.java

--- Fitrition/app/src/main/java/com/example/fitrition/boundary/TrackerFragment.java
+++ Fitrition/app/src/main/java/com/example/fitrition/boundary/TrackerFragment.java
@@ -1,16 +1,22 @@
 package com.example.fitrition.boundary;
 
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
+
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.Toast;
 
 import com.example.fitrition.R;
@@ -78,7 +84,41 @@ public class TrackerFragment extends Fragment {
                 String dateOnly = dateFormat.format(date);
 
                 Toast.makeText(view.getContext(), "Date: " + dateOnly, Toast.LENGTH_LONG).show();
+                LayoutInflater inflater = (LayoutInflater)
+                        view.getContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+                View popupView = inflater.inflate(R.layout.fragment_add_event, null);
 
+                // create the popup window
+                int width = LinearLayout.LayoutParams.MATCH_PARENT;
+                int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+                boolean focusable = true; // lets taps outside the popup also dismiss it
+                final PopupWindow popupWindow = new PopupWindow(popupView, width,  height, focusable);
+
+                // show the popup window
+                // which view you pass in doesn't matter, it is only used for the window tolken
+                popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+                EditText eventDate = (EditText) popupView.findViewById(R.id.eventdatebox);
+                EditText eventMonth = (EditText) popupView.findViewById(R.id.eventmonthbox);
+                EditText eventYear = (EditText) popupView.findViewById(R.id.eventyearbox);
+                SimpleDateFormat dateFormatDay= new SimpleDateFormat("dd ");
+                String forDay = dateFormatDay.format(date);
+                SimpleDateFormat dateFormatMonth= new SimpleDateFormat("MM");
+                String forMonth = dateFormatMonth.format(date);
+                SimpleDateFormat dateFormatYear= new SimpleDateFormat("yyyy");
+                String forYear = dateFormatYear.format(date);
+                eventDate.setText(forDay);
+                eventMonth.setText(forMonth);
+                eventYear.setText(forYear);
+
+                // dismiss the popup window when touched
+                popupView.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        popupWindow.dismiss();
+                        return true;
+                    }
+                });
 
                 colorId = ((ColorDrawable) view.getBackground()).getColor();
                 view.setBackgroundColor(Color.parseColor("#326e62"));
