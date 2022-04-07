17b3a7fc9ee07b3bae056f2bd27ae21543459d09
wangsishi <544wangsishi@gmail.com>
2022-04-07 14:01:45 +0800

    Friend list page with populated friend user profiles

--- Fitrition/app/src/main/java/com/example/fitrition/boundary/FriendListActivity.java
+++ Fitrition/app/src/main/java/com/example/fitrition/boundary/FriendListActivity.java
@@ -1,17 +1,72 @@
 package com.example.fitrition.boundary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.example.fitrition.R;
+import com.example.fitrition.adapter.FriendListRecyclerAdapter;
+import com.example.fitrition.entities.Achievement;
+import com.example.fitrition.entities.Friend;
+import com.example.fitrition.entities.Status;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FriendListActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    LinearLayoutManager layoutManager;
+    List<Friend> userList;
+    FriendListRecyclerAdapter friend_list_adapter;
+    ImageView back_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend_list);
+
+        initData();
+        initRecylerView();
+        back_button=findViewById(R.id.friendlist_back_icon);
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+
+    private void initData() {
+        userList = new ArrayList<>();
+        userList.add(new Friend(R.drawable.ic_avocado_profile, "Jannie", "Hello!",new ArrayList<Status>(), new ArrayList<Achievement>(), "_____________________________________________"));
+        userList.add(new Friend(R.drawable.ic_avocado_profile, "Tonny", "What's up!",new ArrayList<Status>(), new ArrayList<Achievement>(), "_____________________________________________"));
+        userList.add(new Friend(R.drawable.ic_avocado_profile, "Jaden", "Looking for a run!",new ArrayList<Status>(), new ArrayList<Achievement>(), "_____________________________________________"));
+        userList.add(new Friend(R.drawable.ic_avocado_profile, "Jia Xin", "Looking for a gym buddy!",new ArrayList<Status>(), new ArrayList<Achievement>(), "_____________________________________________"));
+        userList.add(new Friend(R.drawable.ic_avocado_profile, "Darren", "Follow my instagram: darren_love_food",new ArrayList<Status>(), new ArrayList<Achievement>(), "_____________________________________________"));
+        userList.add(new Friend(R.drawable.ic_avocado_profile, "Jannie", "Hello!",new ArrayList<Status>(), new ArrayList<Achievement>(), "_____________________________________________"));
+        userList.add(new Friend(R.drawable.ic_avocado_profile, "Tonny", "What's up!",new ArrayList<Status>(), new ArrayList<Achievement>(), "_____________________________________________"));
+        userList.add(new Friend(R.drawable.ic_avocado_profile, "Jaden", "Looking for a run!",new ArrayList<Status>(), new ArrayList<Achievement>(), "_____________________________________________"));
+        userList.add(new Friend(R.drawable.ic_avocado_profile, "Jia Xin", "Looking for a gym buddy!",new ArrayList<Status>(), new ArrayList<Achievement>(), "_____________________________________________"));
+        userList.add(new Friend(R.drawable.ic_avocado_profile, "Darren", "Follow my instagram: darren_love_food",new ArrayList<Status>(), new ArrayList<Achievement>(), "_____________________________________________"));
+    }
+
+    private void initRecylerView() {
+        recyclerView=findViewById(R.id.friendlist_recyclerview);
+        layoutManager=new LinearLayoutManager(this);
+        layoutManager.setOrientation(RecyclerView.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
+        friend_list_adapter=new FriendListRecyclerAdapter(userList);
+        recyclerView.setAdapter(friend_list_adapter);
+        friend_list_adapter.notifyDataSetChanged();
+
     }
 }
