4c2dd8158b422054d663474108f050d964bc8854
FlyingCoconut1 <92644292+FlyingCoconut1@users.noreply.github.com>
2022-04-10 05:47:19 +0800

    Finalized

--- Fitrition/app/src/main/java/com/example/fitrition/control/FacilityManager.java
+++ Fitrition/app/src/main/java/com/example/fitrition/control/FacilityManager.java
@@ -1,60 +1,27 @@
 package com.example.fitrition.control;
-import static android.content.ContentValues.TAG;
 
-import android.net.Uri;
 import android.util.Log;
-import android.view.View;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
-import com.example.fitrition.entities.Facility;
+import com.example.fitrition.ViewFitnessActivity;
+import com.example.fitrition.ViewHawkerActivity;
 import com.example.fitrition.entities.Fitness;
-import com.example.fitrition.entities.FitnessCentreJSON;
 import com.example.fitrition.entities.Food;
-import com.example.fitrition.entities.Review;
+import com.example.fitrition.entities.HawkerCentre;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-import com.google.gson.Gson;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.lang.reflect.Array;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Vector;
 
 public class FacilityManager {
-    private static FacilityManager instance=null;
-    private ArrayList<FitnessCentreJSON> facilityList;
-
-    public ArrayList<Food> getFoodList() {
-        return foodList;
-    }
-
-    public void setFoodList(ArrayList<Food> foodList) {
-        this.foodList = foodList;
-    }
-
-    private ArrayList<Food> foodList;
     private final float distanceInKm = 2;
 
-    private DatabaseReference mDatabaseReference;
-    private StorageReference mStorageReference;
+    private static FacilityManager instance=null;
+    private ArrayList<HawkerCentre> hawkerList;
+    private ArrayList<Food> foodList;
 
     public  FacilityManager(){
-        facilityList = new ArrayList<FitnessCentreJSON>();
+        hawkerList = new ArrayList<HawkerCentre>();
     }
 
     public static FacilityManager getInstance() {
@@ -64,13 +31,22 @@ public class FacilityManager {
         return instance;
     }
 
-    public ArrayList<FitnessCentreJSON> getFacilityList() {
-        return facilityList;
+    public ArrayList<HawkerCentre> getFacilityList() {
+        return hawkerList;
     }
 
-    public void setFacilityList(ArrayList<FitnessCentreJSON> facilityList) {
-        this.facilityList = facilityList;
+    public void setFacilityList(ArrayList<HawkerCentre> hawkerList) {
+        this.hawkerList = hawkerList;
     }
+
+    public ArrayList<Food> getFoodList() {
+        return foodList;
+    }
+
+    public void setFoodList(ArrayList<Food> foodList) {
+        this.foodList = foodList;
+    }
+
 
     public void obtainFacilityBasedOnDistance(Vector<MarkerOptions> markerOptions, double latCur, double lngCur) {
 
@@ -132,104 +108,46 @@ public class FacilityManager {
         return(c * r);
     }
 
-    //Add facility to rtdb
-    //Add facility image to
+    //THe legit Stuff
+    public HawkerCentre searchHawker(String name){
 
-    //Get array of facility objects
-    public void getFacilityImage(FitnessCentreJSON facility){
-        mDatabaseReference = FirebaseDatabase.getInstance("https://fitrition-3a967-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("facilities");
-        mStorageReference = FirebaseStorage.getInstance("gs://fitrition-3a967.appspot.com/").getReference("facility_images");
-        String fileName = facility.getName().replace(' ', '-');
-        StorageReference imageReference = mStorageReference.child(fileName
-                +"."+"jpg");
-        imageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-            @Override
-            public void onSuccess(Uri uri) {
-                String url = uri.toString();
-            }
-        });
-    }
-
-    public void addFacilities (ArrayList<FitnessCentreJSON> facilityArray){
-        mDatabaseReference = FirebaseDatabase.getInstance("https://fitrition-3a967-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("facilities");
-        mStorageReference = FirebaseStorage.getInstance("gs://fitrition-3a967.appspot.com/").getReference("facility_images");
-        for (FitnessCentreJSON facility:facilityArray){
-            mDatabaseReference.child(facility.getName()).setValue(facility).addOnCompleteListener(new OnCompleteListener<Void>() {
-                @Override
-                public void onComplete(@NonNull Task<Void> task) {
-                    Log.d(TAG, "onComplete: added facility"+facility.getName());
-                }
-            });
-        }
-    }
-
-    public void loadFacilities(){
-        facilityList = new ArrayList<FitnessCentreJSON>();
-        mDatabaseReference = FirebaseDatabase.getInstance("https://fitrition-3a967-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("facilities");
-        mStorageReference = FirebaseStorage.getInstance("gs://fitrition-3a967.appspot.com/").getReference("facility_images");
-        mDatabaseReference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for (DataSnapshot childsnap:snapshot.getChildren()){
-                    FitnessCentreJSON facility = childsnap.getValue(FitnessCentreJSON.class);
-                    facilityList.add(facility);
-                    /*mStorageReference.child(facility.getName().replace(' ', '-') + ".jpg").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                        @Override
-                        public void onSuccess(Uri uri) {
-                            facility.setImageUrl(uri.toString());
-                            Log.d(TAG, "onDataChange: "+facility.getImageUrl());
-                            facilityList.add(facility);
-                        }
-                    });*/
-                }
-                mDatabaseReference.removeEventListener(this);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-    }
-
-    public void loadFood(){
-        foodList = new ArrayList<Food>();
-        mDatabaseReference = FirebaseDatabase.getInstance("https://fitrition-3a967-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("foodItems");
-        mDatabaseReference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for (DataSnapshot childsnap:snapshot.getChildren()){
-                    Food f = childsnap.getValue(Food.class);
-                    foodList.add(f);
-
-                }
-                mDatabaseReference.removeEventListener(this);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-    }
-
-    public FitnessCentreJSON searchFacility(String name){
-
-        for(FitnessCentreJSON info : facilityList){
+        ArrayList <HawkerCentre> hawkerList = new ViewHawkerActivity().getAllHawkerArrayList();
+        for(HawkerCentre info : hawkerList){
             if( name.equalsIgnoreCase(info.getName())){
                 return info;
             }
 
         }
 
-        //BB
         return null;
     }
 
-    public void saveFacilitiesnow(){
-        mDatabaseReference = FirebaseDatabase.getInstance("https://fitrition-3a967-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("facilities");
-        mDatabaseReference.setValue(facilityList);
+    public Fitness searchFitness(String name){
+        ArrayList <Fitness> fitnessList = new ViewFitnessActivity().getAllFitnessArrayList();
+
+        for(Fitness info : fitnessList){
+            if( name.equalsIgnoreCase(info.getName())){
+                return info;
+            }
+
+        }
+
+        return null;
     }
+
+    //Return array of partial food
+    public ArrayList<Food> searchForFoodOfferedByHawker(String nameOfHawkerLocation){
+
+        ArrayList <HawkerCentre> hawkerList = new ViewHawkerActivity().getAllHawkerArrayList();
+        for(HawkerCentre info : hawkerList){
+
+            if( nameOfHawkerLocation.equalsIgnoreCase(info.getName()) && info.getFoodList() != null){
+                return info.getFoodList();
+            }
+        }
+        return null;
+    }
+
 }
 
 
