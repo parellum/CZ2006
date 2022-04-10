790585bb2f5fb5b6ada68f549d7e187fa1bab5ed
Jac <80085511+parellum@users.noreply.github.com>
2022-04-10 18:34:54 +0800

    image

--- Fitrition/app/src/main/java/com/example/fitrition/boundary/ProfileFragment.java
+++ Fitrition/app/src/main/java/com/example/fitrition/boundary/ProfileFragment.java
@@ -1,9 +1,13 @@
 package com.example.fitrition.boundary;
 
+import static android.app.Activity.RESULT_OK;
 import static android.content.ContentValues.TAG;
 
+import android.app.Dialog;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -15,6 +19,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -22,8 +27,19 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.example.fitrition.FriendActivity;
 import com.example.fitrition.R;
 import com.example.fitrition.control.ProfileManager;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 import java.time.LocalDate;
 import java.time.Period;
@@ -38,8 +54,14 @@ public class ProfileFragment extends Fragment {
     EditText password;
     ProfileManager profileManager;
     ImageView profilePic;
+    private Uri mImageUri;
+
+    StorageReference mStorageRef;
+    DatabaseReference mDatabaseRef;
 
     int update=0;
+
+    private static final int PICK_IMAGE_REQUEST = 1;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -65,6 +87,16 @@ public class ProfileFragment extends Fragment {
         password = (EditText) view.findViewById(R.id.profile_user_password);
 
         profilePic = (ImageView) view.findViewById(R.id.profile_user_image);
+
+        mStorageRef = FirebaseStorage.getInstance("gs://fitrition-3a967.appspot.com/").getReference("imageupload");
+        mDatabaseRef = FirebaseDatabase.getInstance("https://fitrition-3a967-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("users");
+
+        profilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openFileChooser();
+                }
+        });
 
         changePassword.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -105,6 +137,56 @@ public class ProfileFragment extends Fragment {
 
     }
 
+    private void openFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==PICK_IMAGE_REQUEST && resultCode==RESULT_OK
+                && data!=null && data.getData()!=null){
+            mImageUri=data.getData();
+            Picasso.get().load(mImageUri).into(profilePic);
+
+            Dialog dialog = new Dialog(getContext());
+            dialog.setTitle("Change Profile Picture");
+            dialog.setContentView(R.layout.fragment_invitation);
+            dialog.show();
+
+            Button btnAccept = dialog.findViewById(R.id.acceptBtn);
+            Button btnDecline = dialog.findViewById(R.id.declineBtn);
+            TextView popquizz = dialog.findViewById(R.id.popUpTitle);
+
+            popquizz.setText("Do you want to keep your change?");
+
+            btnAccept.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    uploadFile(FirebaseAuth.getInstance().getCurrentUser().getUid());
+                    dialog.dismiss();
+                }
+            });
+
+            btnDecline.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Glide.with(view.getContext())
+                            .load(profileManager.getUser().getImageUrl())
+                            .into(profilePic);
+                    dialog.dismiss();
+                }
+            });
+
+        }
+    }
+
+
+
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -129,5 +211,46 @@ public class ProfileFragment extends Fragment {
         Glide.with(view.getContext())
                 .load(profileManager.getUser().getImageUrl())
                 .into(profilePic);
+    }
+
+    private String getFileExtension(Uri uri){
+        ContentResolver cR = getView().getContext().getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cR.getType(uri));
+    }
+
+    private void uploadFile(String uid){
+        if (mImageUri!=null){
+            StorageReference fileReference = mStorageRef.child(uid
+                    +"."+getFileExtension(mImageUri));
+
+            fileReference.putFile(mImageUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            fileReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    String url = uri.toString();
+                                    mDatabaseRef.child(uid).child("imageUrl").setValue(url);
+                                }
+                            });
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getView().getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                            Toast.makeText(getView().getContext(), "Uploading...", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }else{
+            Toast.makeText(getView().getContext(),"No file selected!",Toast.LENGTH_SHORT);
+        }
     }
 }
